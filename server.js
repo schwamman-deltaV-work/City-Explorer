4eb08e49344a59b3ad0bc08e616d53b24c31f4d6
Jonathon Schwamman <schwamman@msn.com>
2019-08-19 15:25:44 -0500

    Added data files and created server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Hi, I\'m the server. I will fulfill all your commands.');
+});
