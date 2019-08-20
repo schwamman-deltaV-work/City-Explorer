8ae21d9f88f41ab213cf33c6cbe47b6b80fc7a52
Jonathon Schwamman <schwamman@msn.com>
2019-08-20 11:27:52 -0500

    Added constructor function and GET route for the weather resource

--- server.js
+++ server.js
@@ -23,12 +23,28 @@ function Location(query, geoData){
   this.formatted_query = geoData.results[0].formatted_address;
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
-  
+}
+
+function Weather(weatherData, i) {
+  this.forecast = weatherData.daily.data[i].summary;
+  this.time = convertTime();
+  function convertTime() {
+    return new Date(weatherData.daily.data[i].time).toString().split(' ').slice(0, 4).join(' ');
+  }
 }
 
 app.get('/weather', (request, response) => {
-  const weatherData = require('./data/darksky.json');
-  response.send(weatherData);
+  try {
+    const weatherData = require('./data/darksky.json');
+    const weather = [];
+    for (let i = 0; i < weatherData.daily.data.length; i++) {
+      let hourlyWeather = new Weather(weatherData, i);
+      weather.push(hourlyWeather);
+    }
+    response.send(weather);
+  } catch(error) {
+    response.status(500).send('Dis website is broke. Call someone who cares.');
+  }
 });
 
 const PORT = process.env.PORT || 3000;
